fe325fb69a20ba6630822ae8bfca932f2f909a3b
Nishat2708 <nishantkumar.patel105184@marwadiuniversity.ac.in>
2023-02-01 18:03:39 +0530

    api_key

--- src/shared/axios.ts
+++ src/shared/axios.ts
@@ -2,10 +2,12 @@ import axios from "axios";
 import { API_URL } from "./constants";
 // console.log("abcd", process.env.REACT_APP_API_KEY);
 
+const API_KEY = '1cc28d7cb8202fa7566afa90c4a8b9f4'
+
 const instance = axios.create({
   baseURL: API_URL,
   params: {
-    api_key: process.env.REACT_APP_API_KEY,
+    api_key: API_KEY,
   },
 });
 
