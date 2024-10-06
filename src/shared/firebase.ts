1cd5cec9d686678f432b191ec0501315ae4e7d0a
nishant7929 <nishantpatel7929@gmail.com>
2024-10-06 21:19:14 +0530

    Update firebase configs

--- src/shared/firebase.ts
+++ src/shared/firebase.ts
@@ -2,22 +2,23 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// const app = initializeApp({
-//   apiKey: process.env.REACT_APP_apiKey,
-//   authDomain: process.env.REACT_APP_authDomain,
-//   projectId: process.env.REACT_APP_projectId,
-//   storageBucket: process.env.REACT_APP_storageBucket,
-//   messagingSenderId: process.env.REACT_APP_messagingSenderId,
-//   appId: process.env.REACT_APP_appId,
-// });
+// const firebaseConfig = {
+// 	apiKey: "AIzaSyBiO4NTttc68zMHFkRFXyK5qLsU9zXsJmg",
+// 	authDomain: "moonlight-40ecf.firebaseapp.com",
+// 	projectId: "moonlight-40ecf",
+// 	storageBucket: "moonlight-40ecf.appspot.com",
+// 	messagingSenderId: "1043878072437",
+// 	appId: "1:1043878072437:web:0fd78f6e64f3cfbe2d283c",
+// };
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBiO4NTttc68zMHFkRFXyK5qLsU9zXsJmg",
-  authDomain: "moonlight-40ecf.firebaseapp.com",
-  projectId: "moonlight-40ecf",
-  storageBucket: "moonlight-40ecf.appspot.com",
-  messagingSenderId: "1043878072437",
-  appId: "1:1043878072437:web:0fd78f6e64f3cfbe2d283c",
+	apiKey: process.env.REACT_APP_FIREBASE_apiKey,
+	authDomain: process.env.REACT_APP_FIREBASE_authDomain,
+	projectId: process.env.REACT_APP_FIREBASE_projectId,
+	storageBucket: process.env.REACT_APP_FIREBASE_storageBucket,
+	messagingSenderId: process.env.REACT_APP_FIREBASE_messagingSenderId,
+	appId: process.env.REACT_APP_FIREBASE_appId,
+	measurementId: process.env.REACT_APP_FIREBASE_measurementId,
 };
 
 // const firebaseConfig = {
