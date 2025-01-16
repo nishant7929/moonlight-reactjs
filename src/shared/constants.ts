7877b027ed007e8ab3420d02e4f2e400cb6eac93
Nishant Patel <nishantpatel7929@gmail.com>
2025-01-16 21:53:07 +0530

    Add render api for proxy

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -1,7 +1,9 @@
 export const API_URL = 'https://api.themoviedb.org/3';
-export const PROXY_API_URL = 'https://andshop-mern-api.vercel.app/api/moviedb';
+// export const PROXY_API_URL = 'https://andshop-mern-api.vercel.app/api/moviedb';
+export const PROXY_API_URL = 'https://andshop-mern-lh2b.onrender.com/api/moviedb';
 export const IMAGE_URL = 'https://image.tmdb.org/t/p';
-export const PROXY_IMAGE_URL = 'https://andshop-mern-api.vercel.app/moviedb-img';
+// export const PROXY_IMAGE_URL = 'https://andshop-mern-api.vercel.app/moviedb-img';
+export const PROXY_IMAGE_URL = 'https://andshop-mern-lh2b.onrender.com/moviedb-img';
 export const EMBED_URL = 'https://2embed.org/embed';
 export const EMBED_VIDSRC = 'https://vidsrc.me/embed';
 export const EMBED_TO = 'https://www.2embed.to/embed/tmdb';
